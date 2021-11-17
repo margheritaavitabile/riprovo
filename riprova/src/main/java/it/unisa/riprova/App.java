10425ff06c0952e4b234efabe7ff5122557d5e66
meggy <meggy@host.docker.internal>
2021-11-17 15:10:30 +0100

    commit 2

--- riprova/src/main/java/it/unisa/riprova/App.java
+++ riprova/src/main/java/it/unisa/riprova/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
 
     private static Scene scene;
 
+    
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
